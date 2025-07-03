d0fad249d1b149e7a25d7b243a19a7e8831dfe83
bnjpcson <pecsonbenjiea@gmail.com>
2025-07-04 00:48:03 +0800

    Added: Caching techniques in Next JS

--- app/actions.ts
+++ app/actions.ts
@@ -3,6 +3,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { prisma } from "./utils/db";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export async function handleSubmission(formData: FormData) {
   const { getUser } = getKindeServerSession();
@@ -26,6 +27,6 @@ export async function handleSubmission(formData: FormData) {
       authorImage: user?.picture as string,
     },
   });
-
+  revalidatePath("/");
   return redirect("/dashboard");
 }
